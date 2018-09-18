bc16d86b16c600906dde508758e559967675bf5e
limbo <limbo_china@163.com>
2018-09-18 13:00:51 +0800

    9-18

---
+++ 7_Reverse_Integer/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverse(int x) {
+        int res =0;
+        while(x!=0){
+            res = res*10 + x%10;
+            x = x/10;
+        }
+        return res;
+    }
+}
